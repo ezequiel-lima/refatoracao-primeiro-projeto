aa6160a57c41d5c3264bad66470568623f1c1b56
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-08-16 23:32:03 -0300

    Extarir variavel

--- refatoracao/Program.cs
+++ refatoracao/Program.cs
@@ -6,6 +6,18 @@ namespace refatoracao
     {
         static void Main(string[] args)
         {
+            //// Exercicio de extrair variavel
+            //void GerarListagem()
+            //{
+            //    bool ehMac = (plataforma.ToUpper().IndexOf("MAC") > -1);
+            //    bool ehChrome = (browser.ToUpper().IndexOf("CHROME") > -1);
+            //    bool foiRedimensionado = redimensionado > 0;
+
+            //    if (ehMac && ehChrome && foiInicializado() && foiRedimensionado)
+            //    {
+            //        // faça alguma coisa
+            //    }
+            //}
         }
     }
 }
